8e56ee62e15e30cd715d9f2429273450c1c9fd76
Gael Miranday <gaelmiranday08@gmail.com>
2023-12-11 15:25:16 +0100

    second commit

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
     const tasks = [
         { title: "Payer mes factures", priority: 1 },
         { title: "Faire la vaisselle", priority: 2 },
         { title: "Vider les poubelles", priority: 3 }
     ];
 
+    //  tâches dans la liste 
     function displayTasks() {
         const taskList = document.querySelector('ul');
         let html = '';
 
         tasks.forEach(task => {
-
             switch (task.priority) {
                 case 1:
                     html += `<li style="color: red;"><label><input type="checkbox">${task.title}</label></li>`;
@@ -27,9 +26,32 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
 
-
         taskList.innerHTML = html;
     }
 
+    // ajouter une nouvelle tâche
+    function addTask(title, priority) {
+        tasks.push({ title, priority });
+        displayTasks(); 
+    }
+
+    
     displayTasks();
+
+    //  formulaire
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+
+      
+        const title = document.getElementById('title').value;
+        const priority = document.getElementById('priority').value;
+
+        
+        addTask(title, parseInt(priority));
+
+       
+        form.reset();
+    });
 });
+
